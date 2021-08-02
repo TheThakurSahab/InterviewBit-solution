f38c2fb4188b2342918f7170267ac8594d98e52a
Akash Singh <thethakursaahab@outlook.com>
2021-08-02 21:00:59 +0530

    Create Rearrange Array.java

---
+++ Math/Rearrange Array.java
@@ -0,0 +1,21 @@
+Rearrange a given array so that Arr[i] becomes Arr[Arr[i]] with O(1) extra space.
+
+Example:
+
+Input : [1, 0]
+Return : [0, 1]
+Lets say N = size of the array. Then, following holds true :
+
+All elements in the array are in the range [0, N-1]
+
+
+public class Solution {
+	public void arrange(ArrayList<Integer> A) {
+        int n=A.size();  
+        for(int i=0;i<n;i++){
+            A.set(i,A.get(i)+(A.get(A.get(i))%n)*n);
+        }
+        for(int i=0;i<n;i++)
+        A.set(i,A.get(i)/n);
+	}
+}
