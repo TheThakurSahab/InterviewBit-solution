6270392ac84a9f1ac385f613f0bd602982c9c8e4
Akash Singh <thethakursaahab@outlook.com>
2021-08-05 22:39:26 +0530

    Create Kth Smallest Element.java

---
+++ Arrays/Kth Smallest Element.java
@@ -0,0 +1,42 @@
+Problem Description
+
+Find the Bth smallest element in given array A .
+
+NOTE: Users should try to solve it in <= B swaps.
+
+
+
+Problem Constraints
+1 <= |A| <= 100000
+
+1 <= B <= min(|A|, 500)
+
+1 <= A[i] <= 109
+
+
+
+Input Format
+First argument is vector A.
+
+Second argument is integer B.
+
+
+
+Output Format
+Return the Bth smallest element in given array.
+  
+  public class Solution {
+    public int kthsmallest(final int[] arr, int B) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++){
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+        return arr[B-1];
+    }
+}
