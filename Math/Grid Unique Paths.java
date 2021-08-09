64a613f0a7c4062e90e3811c62a8afed83a60355
Akash Singh <thethakursaahab@outlook.com>
2021-08-09 18:31:43 +0530

    Create Grid Unique Paths.java

---
+++ Math/Grid Unique Paths.java
@@ -0,0 +1,39 @@
+A robot is located at the top-left corner of an A x B grid
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked ‘Finish’ in the diagram below).
+
+How many possible unique paths are there?
+
+Note: A and B will be such that the resulting answer fits in a 32 bit signed integer.
+
+Example :
+
+Input : A = 2, B = 2
+Output : 2
+
+2 possible routes : (0, 0) -> (0, 1) -> (1, 1) 
+              OR  : (0, 0) -> (1, 0) -> (1, 1)
+ 
+                
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1)
+            return 1;
+        m--;
+        n--;
+        if(m < n) {              // Swap, so that m is the bigger number
+            m = m + n;
+            n = m - n;
+            m = m - n;
+        }
+        long res = 1;
+        int j = 1;
+        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
+            res *= i;
+            res /= j;
+        }
+            
+        return (int)res;
+    }
+}
+        
